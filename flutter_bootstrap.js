abf2a3a42e1f035a8c69e17a774e5237c7f6fbde
Andrea Chinello <andrea.chinello94@gmail.com>
2025-06-01 16:38:33 +0200

    Flippable Potion

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2373848619"
+    serviceWorkerVersion: "1242092446"
   }
 });

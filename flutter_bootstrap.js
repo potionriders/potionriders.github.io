1ee3201047e6034ac2d7ee6cffb7a7baa7f91529
Andrea Chinello <andrea.chinello94@gmail.com>
2025-06-29 11:27:12 +0200

    Change to room managmenet and login issue

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1242092446"
+    serviceWorkerVersion: "1433370531"
   }
 });
